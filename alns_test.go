853d87038189b709cc5f7e954481430261a8e7f0
bibenga <{ID}+{username}@users.noreply.github.com>
2025-08-22 14:13:48 +0000

    fix tests

--- alns_test.go
+++ alns_test.go
@@ -21,6 +21,8 @@ func TestAlns(t *testing.T) {
 	a := NewWithPCGRandom(1, 2)
 	a.CollectObjectives = true
 
+	lastBest := rand.Float64()
+	bestCount := 0
 	destroyCalled := 0
 	a.AddDestroyOperator(func(state State, rnd *rand.Rand) State {
 		destroyCalled++
@@ -33,35 +35,41 @@ func TestAlns(t *testing.T) {
 		repairCalled++
 		current := state.(*FakeState)
 		current.objective = rand.Float64()
+		if current.objective < lastBest {
+			lastBest = current.objective
+			bestCount++
+		}
 		return current
 	})
 
-	initialSolution := FakeState{objective: 1}
+	total := 10000
+
+	initialSolution := FakeState{objective: lastBest}
 	opSelect, _ := NewRouletteWheel([4]float64{3, 2, 1, 0.5}, 0.8, 1, 1, nil)
 	accept := HillClimbing{}
-	stop := MaxIterations{MaxIterations: 10}
+	stop := MaxIterations{MaxIterations: total}
 	res := a.Iterate(&initialSolution, &opSelect, &accept, &stop)
 
-	if destroyCalled != 10 {
-		t.Fatalf("10 destroy calls expected, actual %d calls", destroyCalled)
+	if destroyCalled != total {
+		t.Errorf("%d destroy calls expected, actual %d calls", total, destroyCalled)
 	}
-	if repairCalled != 10 {
-		t.Fatalf("10 repair calls expected, actual %d calls", repairCalled)
+	if repairCalled != total {
+		t.Errorf("%d repair calls expected, actual %d calls", total, repairCalled)
 	}
-	if stop.currentIteration != 11 {
-		t.Fatalf("10 iterations expected, actual %d calls", stop.currentIteration)
+	if stop.currentIteration != total+1 {
+		t.Errorf("%d iterations expected, actual %d calls", total+1, stop.currentIteration)
 	}
-	if len(res.Statistics.Objectives) != 11 {
-		t.Fatalf("11 objectives expected, actual %d objectives", len(res.Statistics.Objectives))
+	if len(res.Statistics.Objectives) != total+1 {
+		t.Errorf("%d objectives expected, actual %d objectives", total+1, len(res.Statistics.Objectives))
 	}
-	repairOperatorCounts := OperatorStatistics{3, 0, 0, 7}
+	repairOperatorCounts := OperatorStatistics{bestCount, 0, 0, total - bestCount}
 	if res.Statistics.RepairOperatorCounts[0] != repairOperatorCounts {
-		t.Fatalf("expected repair opeator statistics %v, actual %v",
+		t.Errorf("expected repair opeator statistics %v, actual %v",
 			repairOperatorCounts, res.Statistics.RepairOperatorCounts[0])
 	}
-	rejectOperatorCounts := OperatorStatistics{3, 0, 0, 7}
+	rejectOperatorCounts := OperatorStatistics{bestCount, 0, 0, total - bestCount}
 	if res.Statistics.DestroyOperatorCounts[0] != rejectOperatorCounts {
-		t.Fatalf("expected destory opeator statistics %v, actual %v",
+		t.Errorf("expected destory opeator statistics %v, actual %v",
 			rejectOperatorCounts, res.Statistics.DestroyOperatorCounts[0])
 	}
 }
